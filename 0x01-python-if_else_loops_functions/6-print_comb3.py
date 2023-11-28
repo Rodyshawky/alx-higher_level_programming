db6190123f4b2fad45ce7b6f052694b207e42010
rodyshawky <rodashawkei@gmail.com>
2023-11-28 10:42:51 -0800

    task6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,6 +4,6 @@ for i in range(10):
         if i == j:
             continue
         elif i == 8 and j == 9:
-            print("{:d}{:d}".format(i,j))
+            print("{:d}{:d}".format(i, j))
         else:
-            print("{:d}{:d}, ".format(i,j), end="")
+            print("{:d}{:d}, ".format(i, j), end="")
