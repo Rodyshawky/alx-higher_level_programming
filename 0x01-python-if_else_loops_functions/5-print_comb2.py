8264f31eefecd30ae1344e5f78b741c0c16d38c4
rodyshawky <rodashawkei@gmail.com>
2023-11-28 10:13:36 -0800

    task5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(9):
-    print("0{:d}, ".format(i), end="")
-for n in range (10 , 99):
-    print("{:d}, ".format(n), end="")
+for i in range(100):
+    if i < 10:
+        print("0{:d}, ".format(i), end="")
+    else:
+        print("{:d}, ".format(i), end="")
