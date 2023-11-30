d21ee51a1b1a342c5497f55819f4a17ce8012247
rodyshawky <rodashawkei@gmail.com>
2023-11-30 14:36:39 -0800

    task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     if counter == 0:
         print("0 arguments.")
     elif counter == 1:
-        print("1 arguments:")
+        print("1 argument:")
     else:
         print("{} arguments:".format(counter))
 for i in range(counter):
