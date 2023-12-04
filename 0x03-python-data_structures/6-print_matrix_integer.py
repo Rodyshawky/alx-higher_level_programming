c8e1397e4859672e4cad08b023d026c684cefc98
rodyshawky <rodashawkei@gmail.com>
2023-12-04 09:34:21 -0800

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
         return None
     for row in range(len(matrix)):
         for column in range(len(matrix[0])):
-            print("{:d}".format(matrix[row][column]), end=" ")
+            print("{:d}".format(matrix[row][column]), end="")
         print("")
