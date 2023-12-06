7016f675ffff911d3bebc09f1e4b7e093cbbc532
rodyshawky <rodashawkei@gmail.com>
2023-12-06 15:31:20 -0800

    task10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    else:
+        max_v = 0
+        max_key = None
+        for k, v in a_dictionary.items():
+            if v > max_v:
+                max_v = v
+                max_key = k
+        return max_key
