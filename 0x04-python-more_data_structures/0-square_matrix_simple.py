dcf363c7244a36673f8bb01a041812783f6af2d5
rodyshawky <rodashawkei@gmail.com>
2023-12-06 05:44:59 -0800

    task0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    sq = list(map(lambda submat: list(map(lambda x : x ** 2, submat)), matrix))
+    sq = list(map(lambda submat: list(map(lambda x: x ** 2, submat)), matrix))
     return sq
