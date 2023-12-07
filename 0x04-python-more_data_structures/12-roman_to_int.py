30be709c62cce1a0396ac9ad5bc796ced8c383bb
rodyshawky <rodashawkei@gmail.com>
2023-12-06 16:00:51 -0800

    task12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,10 +5,8 @@ def roman_to_int(roman_string):
     else:
         res = 0
         n = 0
-        dic = {'I': 1, 'V': 5, 'X': 10,
-            'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         for r in reversed(roman_string):
             n = dic[r]
             res += n if res < n * 5 else -n
         return res
-
