1adeeb6c3f3940d4008dc2bdebe083906914a141
rodyshawky <rodashawkei@gmail.com>
2023-12-18 10:26:31 -0800

    task2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,7 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]))
             i += 1
             count += 1
-    except IndexError:
+    except (IndexError, TypeError):
         None
     return count
 
