dfc90b6b0bf071c3a2ee4aa329773f8d0b5bbc02
rodyshawky <rodashawkei@gmail.com>
2023-12-18 10:24:28 -0800

    task2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    count = 0
+    try:
+        while i != x:
+            print("{:d}".format(my_list[i]))
+            i += 1
+            count += 1
+    except IndexError:
+        None
+    return count
+
