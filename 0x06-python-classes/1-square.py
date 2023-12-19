4e7837c4741bb3962b121a76d589cfcca14954b5
rodyshawky <rodashawkei@gmail.com>
2023-12-19 09:14:57 -0800

    task2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Define Module"""
+
 
 class Square:
+    """Define square class"""
     def __init__(self, size):
+        """define instance attribute"""
         self.__size = size
