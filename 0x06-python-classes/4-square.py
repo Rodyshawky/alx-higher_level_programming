7e99c35cd8a84bcc2c5ee4b848f626ab1e0cb37b
rodyshawky <rodashawkei@gmail.com>
2023-12-19 10:24:07 -0800

    task4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""define Module"""
+
+
+class Square:
+    """Define class"""
+    def __init__(self, size=0):
+        """initial private size value"""
+        self.__size = size
+        """size getter"""
+    @property
+    def size(self):
+        return self.__size
+    """size setter"""
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if not isinstance(value, int):
+            """Type Error Exception"""
+            raise TypeError("size must be an integer")
+        if value < 0:
+            """Value Error Exception"""
+            raise ValueError("size must be >= 0")
+        """public function"""
+    def area(self):
+        """return square Area"""
+        return self.__size ** 2
