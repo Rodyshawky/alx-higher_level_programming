452242e17f49dc537f7f2fc7d18cf6dd30487212
rodyshawky <rodashawkei@gmail.com>
2023-12-19 12:34:02 -0800

    task6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -8,11 +8,9 @@ class Square:
     def __init__(self, size=0, position=(0, 0)):
         self.__size = size
         self.__position = position
-
     @property
     def size(self):
         return self.__size
-
     """size setter"""
     @size.setter
     def size(self, value):
@@ -23,7 +21,6 @@ class Square:
         if value < 0:
             """Value Error Exception"""
             raise ValueError("size must be >= 0")
-
     """position getter"""
     @property
     def position(self):
