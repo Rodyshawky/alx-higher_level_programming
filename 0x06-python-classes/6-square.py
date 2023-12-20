e74196ef68463244fc057bce9f8ae1977e5f377f
rodyshawky <rodashawkei@gmail.com>
2023-12-20 12:39:18 -0800

    task6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -8,6 +8,7 @@ class Square:
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
+
     @property
     def size(self):
         return self.__size
