e3a31a7239b4b0ddd21b63c390b46f46062719df
rodyshawky <rodashawkei@gmail.com>
2024-01-09 13:45:16 -0800

    task1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,5 +3,4 @@
 
 def write_file(filename="", text=""):
     with open(filename, "w", encoding="UTF-8") as my_file:
-        my_file.write(text)
-        return len(list(filename))
+        return my_file.write(text)
