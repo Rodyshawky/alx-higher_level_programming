9510b54c9a42557f63b8cf3cae8c52e1dec54bf9
rodyshawky <rodashawkei@gmail.com>
2024-01-09 14:16:14 -0800

    task4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -7,6 +7,5 @@ import json
 
 def from_json_string(my_str):
     """function json to string """
-    json_string = json.loads(my_str)
-    return json_string
+    return json.loads(my_str)
 ~                            
