1a968be2b1aec36f818217731aa4f867120af473
rodyshawky <rodashawkei@gmail.com>
2024-03-09 13:28:09 -0800

    advanced

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,6 @@
-- List avergae temperatures
--- Execute: cat 101-avg_temperatures.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-SELECT AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
+-- Displays the average temperature (in Fahrenheit)
+-- by city ordered by descending temperature.
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
